7a200020b4bff14dbb6314989b45b95109dd6a82
MansAlien <mans012033945421@gmail.com>
2025-02-17 12:38:27 +0200

    create the services and the serializers of BankAccount

---
+++ bankaccount/services.py
@@ -0,0 +1,12 @@
+from .models import BankAccount, Transaction
+
+class ManageBankAccount:
+
+    @staticmethod
+    def get_account_by_id(pk):
+        return BankAccount.objects.get(id=pk)
+
+    @staticmethod
+    def create_bank_account(user):
+        bank_account = BankAccount.objects.create(**user)
+        return bank_account
